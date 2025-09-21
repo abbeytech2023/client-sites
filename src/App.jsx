8024ba7793da749aff2c45affa73a25303685eb3
olanipekun abiodun <olanipekunabbey@gmail.com>
2025-09-21 10:59:53 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Ucee from "./components/Ucee";
 import G2global from "./components/G2global";
+import EzeNwanneAutos from "./components/CarDealer";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Ucee />} />
+      <Route path="/eze" element={<EzeNwanneAutos />} />
       <Route path="/g2" element={<G2global />} />
     </Routes>
   );
